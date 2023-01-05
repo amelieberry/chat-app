5106953517f162004489af9a53da1ae1b5042ff5
Amelie <aac.berry@gmail.com>
2023-01-05 15:11:53 +0100

    set message state and render chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,20 +1,71 @@
-import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import { View, Platform, KeyboardAvoidingView } from "react-native";
+
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
 export default function Chat(props) {
+    const [messages, setMessages] = useState([]);
     let { color } = props.route.params;
 
     useEffect(() => {
         // use name prop to set the title in nav to the name entered by the user
         let { name } = props.route.params;
         props.navigation.setOptions({ title: name });
-    });
+
+        // set state of messages
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+            {
+                _id: 2,
+                text: 'This is a system message',
+                createdAt: new Date(),
+                system: true,
+            },
+        ])
+    }, [])
+
+    // when message is sent, append new message to messages state
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    }, [])
+
+    // Customize render bubble
+    function renderBubble(props) {
+        return (
+            <Bubble {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: '#2b2b2b',
+                    }
+                }}
+            />
+        )
+    }
 
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: color}}>
-            <View style={{backgroundColor: 'white', opacity: 0.7, height: '80%', width: '80%', justifyContent: 'center', alignItems: 'center'}}>
-                <Text style={{fontWeight: '600'}}>Chat Page</Text>
-            </View>
+        <View style={{ flex: 1, backgroundColor: color }}>
+            <GiftedChat
+                renderBubble={renderBubble}
+                messages={messages}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+                accessible={true}
+                accessibilityLabel='Chat text input field'
+                accessibilityHint='Enter your message here and press "Send" on the right to sned your message '
+            />
+            {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+            }
         </View>
     )
 }
