28dcdf0c8c5a8bdaaf4545b5fc89e7d901400f88
Amelie <aac.berry@gmail.com>
2023-01-12 18:17:21 +0100

    export AsyncStorage get, add & delete functions

---
+++ asyncStorage.js
@@ -0,0 +1,34 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Get messages from AsyncStorage
+export const getMessages = async () => {
+    try {
+        const messageStringified =
+            (await AsyncStorage.getItem('messages')) || [];
+        const messageParsed = JSON.parse(messageStringified) || [];
+        console.log('AsyncStorage number of messages in Array:', messageParsed.length)
+        console.log('AsyncStorage messages:', messageParsed);
+        return messageParsed;
+    } catch (error) {
+        console.error('get message from storage error', error.message);
+    }
+};
+
+// Add messages to AsyncStorage
+export const saveMessages = async (messages) => {
+    try {
+        await AsyncStorage.setItem('messages', JSON.stringify(messages));
+        console.warn('saved in local storage:', messages)
+    } catch (error) {
+        console.error('save message to storage error', error.message);
+    }
+};
+
+// Delete messages from AsyncStorage
+export const deleteMessages = async () => {
+    try {
+        await AsyncStorage.removeItem('messages');
+    } catch (error) {
+        console.error('remove message from storage error', error.message);
+    }
+};
